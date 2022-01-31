c1005e360cb21f1c51a497ee378d447a54a982f2
Technik Tüftler <technik.tueftler@gmail.com>
2022-01-31 22:13:55 +0100

    try async

--- source/main.py
+++ source/main.py
@@ -2,13 +2,14 @@ from configParser import get_configuration
 from configParser import get_int_element as int_cfg
 import time
 from fritzconnection.lib.fritzstatus import FritzStatus
+import asyncio
 
 read_successful, cfg = get_configuration("fritzbox")
 
 fc = FritzStatus(address=cfg["address"], password=cfg["password"])
 
 
-def check_low_network_load() -> bool:
+async def check_low_network_load() -> bool:
     total_upstream_raw, total_downstream_raw = 0, 0
     for _ in range(int_cfg("function_trigger", "loop_count")):
         upstream_raw, downstream_raw = fc.transmission_rate
@@ -20,17 +21,10 @@ def check_low_network_load() -> bool:
     return True
 
 
-def main():
-    while True:
-        upstream_raw, downstream_raw = fc.transmission_rate
-        print(f"Upstream: {round((upstream_raw))}Byte/s -- Downstream: {round((downstream_raw))}Byte/s")
-        print(f"Upstream: {round((upstream_raw * 8))}Bit/s -- Downstream: {round((downstream_raw * 8))}Bit/s")
-        print(f"Upstream: {round((upstream_raw * 8 / 1024))}KBit/s -- Downstream: {round((downstream_raw * 8 / 1024))}KBit/s")
-        print(f"Upstream: {round((upstream_raw * 8 / 1024 / 1024))}MBit/s -- Downstream: {round((downstream_raw * 8 / 1024 / 1024))}MBit/s")
-        print("-----------------------------------------------------------")
-        time.sleep(2)
+async def main():
+    task2 = asyncio.create_task(check_low_network_load())
+    if await task2 is True:
+        print("Starte Programm")
 
-
-if __name__ == '__main__':
-    # main()
-    check_low_network_load()
+if __name__ == "__main__":
+    asyncio.run(main())
