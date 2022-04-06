22d6035c37f603924941b4629d23cb790dfd73b7
Technik Tüftler <technik.tueftler@gmail.com>
2022-04-06 22:10:22 +0200

    # What - solve https://github.com/Technik-Tueftler/Speedtest/issues/6 - test PostgreSQL and adapt documentation

--- source/db.py
+++ source/db.py
@@ -7,9 +7,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
 from datetime import datetime
+from dataclasses import dataclass
 
 Base = declarative_base()
-session = None
 
 if os.getenv("WORKING_DIR") is None:
     sql_db_path = "sqlite:///../files/measurements.sqlite3"
@@ -28,49 +28,67 @@ class Measurements(Base):
     ping_speedtest = sqlalchemy.Column(sqlalchemy.Integer)
 
 
-def commit(function):
-    def wrapper(*args, **kwargs):
-        return_function = function(*args, **kwargs)
-        session.commit()
-        return return_function
-    return wrapper
+@dataclass
+class SQLAlchemyConnectionManager:
+    connector: str
+    engine: sqlalchemy.engine.Engine = None
+    session_make: sqlalchemy.orm.session.sessionmaker = None
+    session: sqlalchemy.orm.session.Session = None
+
+    def __enter__(self):
+        self.engine = create_engine(self.connector)
+        Base.metadata.bind = self.engine
+        self.session_make = sessionmaker(bind=self.engine)
+        self.session = self.session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.session.close()
+        self.engine.dispose()
+
+    def add(self, data: Measurements):
+        self.session.add(data)
+        self.session.commit()
 
 
-@commit
 def add_measurement(data: dict) -> None:
-    session.add(Measurements(timestamp=datetime.now(),
-                             max_download_fritzbox=data["max_download_fritzbox"],
-                             max_upload_fritzbox=data["max_upload_fritzbox"],
-                             avg_download_speedtest=data["avg_download_speedtest"],
-                             avg_upload_speedtest=data["avg_upload_speedtest"],
-                             ping_speedtest=data["ping_speedtest"]))
+    with SQLAlchemyConnectionManager(sql_db_path) as conn:
+        conn.add(Measurements(timestamp=datetime.now(),
+                              max_download_fritzbox=data["max_download_fritzbox"],
+                              max_upload_fritzbox=data["max_upload_fritzbox"],
+                              avg_download_speedtest=data["avg_download_speedtest"],
+                              avg_upload_speedtest=data["avg_upload_speedtest"],
+                              ping_speedtest=data["ping_speedtest"]))
 
 
 def check_and_verify_database_connection() -> None:
-    global Base
-    global session
     connector = os.getenv("DB_CONNECTOR", sql_db_path)
     try:
         engine = create_engine(connector)
         if not database_exists(engine.url):
             create_database(engine.url)
         Base.metadata.create_all(engine)
-        session_make = sessionmaker(bind=engine)
-        session = session_make()
+        engine.dispose()
 
-    except sqlalchemy.exc.OperationalError as _:
-        print(f"ERROR: No connection to the database is possible. Please check DB_CONNECTOR. The measurements will be "
-              f"stored in a SQLite anyway.")
+    except sqlalchemy.exc.OperationalError as e:
+        print(f"ERROR: No connection to the database is possible. Aborted with error: [{e}]. "
+              f"Please check DB_CONNECTOR. The measurements will be stored in a SQLite anyway.")
         engine = create_engine(sql_db_path)
         Base.metadata.create_all(engine)
-        session_make = sessionmaker(bind=engine)
-        session = session_make()
-    except sqlalchemy.exc.ProgrammingError as _:
-        print(f"ERROR: Unknown")
+        engine.dispose()
+
+    except sqlalchemy.exc.ProgrammingError as e:
+        print(f"ERROR: unexpected error: [{e}].")
 
 
 def main() -> None:
-    pass
+    with SQLAlchemyConnectionManager(sql_db_path) as session:
+        session.add(Measurements(timestamp=datetime.now(),
+                                 max_download_fritzbox=676,
+                                 max_upload_fritzbox=676,
+                                 avg_download_speedtest=676,
+                                 avg_upload_speedtest=676,
+                                 ping_speedtest=676))
 
 
 if __name__ == "__main__":
